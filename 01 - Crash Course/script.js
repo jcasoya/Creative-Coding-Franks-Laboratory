28b1a16cc37a4bc953bab5f8151e9c98ec1f4ea3
jcasoya <jcasoya@gmail.com>
2023-05-16 17:28:25 +0200

    random particle animation on mouse click

--- 01 - Crash Course/script.js
+++ 01 - Crash Course/script.js
@@ -16,6 +16,9 @@ const mouse = {
 canvas.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
+    for (let i = 0; i < 10; i++ ){
+        particlesArray.push(new Particle());
+    }
 })
 
 canvas.addEventListener('mousemove', function(event){
@@ -25,37 +28,35 @@ canvas.addEventListener('mousemove', function(event){
 
 class Particle {
     constructor(){
-        // this.x = mouse.x;
-        // this.y = mouse.y;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-        this.size = Math.random() * 5 + 1;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        // this.x = Math.random() * canvas.width;
+        // this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 15 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
     }
     update(){
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.size > 0.2) this.size -= 0.1;
     }
     draw(){
         ctx.fillStyle = 'blue';
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 20, 0, Math.PI * 1.4);
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 1.4);
         ctx.fill();
     }
 }
-
-function init(){
-    for (let i = 0; i < 100; i++){
-        particlesArray.push(new Particle());
-    }
-}
-init();
 
 function handleParticles(){
     for (let i = 0; i < particlesArray.length; i++){
         particlesArray[i].update();
         particlesArray[i].draw();
+        if (particlesArray[i].size <= 0.3){
+            particlesArray.splice(i, 1);
+            i--;
+        }
     }
 }
 
