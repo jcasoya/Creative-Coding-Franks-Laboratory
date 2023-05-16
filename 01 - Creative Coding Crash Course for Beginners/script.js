4f8ff384616d4dfbdf1d4c8b781e0d590f26cce0
jcasoya <jcasoya@gmail.com>
2023-05-17 00:09:29 +0200

    raibow lightning procedural generating

--- 01 - Creative Coding Crash Course for Beginners/script.js
+++ 01 - Creative Coding Crash Course for Beginners/script.js
@@ -15,7 +15,11 @@ class Line {
         this.history = [{x: this.x, y: this.y}];
         this.lineWidth = Math.floor(Math.random() * 15 + 1);
         this.hue = Math.floor(Math.random() * 360);
-        this.maxLength = 24;
+        this.maxLength = Math.floor(Math.random() * 150 + 10);
+        this.speedX = Math.random() * 1 - 0.5;
+        this.speedY = 7;
+        this.lifeSpan = this.maxLength * 3;
+        this.timer = 0;
     }
     draw(context){
         context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
@@ -28,17 +32,30 @@ class Line {
         context.stroke();
     }
     update(){
-        this.x = Math.random() * this.canvas.width;
-        this.y = Math.random() * this.canvas.height;
-        this.history.push({x: this.x, y: this.y});
-        if (this.history.length > this.maxLength){
+        this.timer++;
+        if (this.timer < this.lifeSpan){
+            this.x += this.speedX + Math.random() * 20 - 10;
+            this.y += this.speedY + Math.random() * 20 - 10;
+            this.history.push({x: this.x, y: this.y});
+            if (this.history.length > this.maxLength){
+                this.history.shift();
+            }
+        } else if (this.history.length <= 1){
+            this.reset();
+        } else {
             this.history.shift();
         }
+    }
+    reset(){
+        this.x = Math.random() * this.canvas.width;
+        this.y = Math.random() * this.canvas.height;
+        this.history = [{x: this.x, y: this.y}];
+        this.timer = 0;
     }
 }
 
 const linesArray = [];
-const numberOfLines = 1;
+const numberOfLines = 20;
 for (let i = 0; i < numberOfLines; i++){
     linesArray.push(new Line(canvas));
 }
