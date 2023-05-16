5c6acbf25e6f3d57f766bcc7fa35ef3b440ff63e
jcasoya <jcasoya@gmail.com>
2023-05-16 21:02:11 +0200

    animating lines

--- 01 - Creative Coding Crash Course for Beginners/script.js
+++ 01 - Creative Coding Crash Course for Beginners/script.js
@@ -15,21 +15,25 @@ class Line {
         this.history = [{x: this.x, y: this.y}];
         this.lineWidth = Math.floor(Math.random() * 15 + 1);
         this.hue = Math.floor(Math.random() * 360);
+        this.maxLength = 24;
     }
     draw(context){
         context.strokeStyle = 'hsl(' + this.hue + ', 100%, 50%)';
         context.lineWidth = this.lineWidth;
         context.beginPath();
         context.moveTo(this.history[0].x, this.history[0].y);
-        for (let i = 0; i < 12; i++){
-            this.x = Math.random() * this.canvas.width;
-            this.y = Math.random() * this.canvas.height;
-            this.history.push({x: this.x, y: this.y});
-        }
         for (let i = 0; i < this.history.length; i++){
             context.lineTo(this.history[i].x, this.history[i].y);
         }
         context.stroke();
+    }
+    update(){
+        this.x = Math.random() * this.canvas.width;
+        this.y = Math.random() * this.canvas.height;
+        this.history.push({x: this.x, y: this.y});
+        if (this.history.length > this.maxLength){
+            this.history.shift();
+        }
     }
 }
 
@@ -40,4 +44,15 @@ for (let i = 0; i < numberOfLines; i++){
 }
 
 console.log(linesArray);
-linesArray.forEach(line => line.draw(ctx));
+
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //draw line
+    linesArray.forEach(line => {
+        line.draw(ctx)
+        line.update()
+    });
+    requestAnimationFrame(animate);
+}
+animate();
