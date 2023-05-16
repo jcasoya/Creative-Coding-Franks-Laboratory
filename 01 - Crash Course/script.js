8ad5da6e1326822bf1ed1708ebdd4e5ab4d345dd
jcasoya <jcasoya@gmail.com>
2023-05-16 16:59:16 +0200

    define particle class

--- 01 - Crash Course/script.js
+++ 01 - Crash Course/script.js
@@ -9,25 +9,41 @@ window.addEventListener('resize', function(){
 })
 
 const mouse = {
-    x: null,
-    y: null,
+    x: undefined,
+    y: undefined,
 }
 canvas.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    drawCircle();
 })
 
 canvas.addEventListener('mousemove', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    drawCircle();
 })
 
-function drawCircle(){
-    ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, 20, 0, Math.PI * 1.4);
-    ctx.fill();
+class Particle {
+    constructor(){
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 20, 0, Math.PI * 1.4);
+        ctx.fill();
+    }
 }
-// drawCircle();
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(animate);
+}
+animate();
