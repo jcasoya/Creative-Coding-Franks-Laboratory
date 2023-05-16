f75c29e367dae4abf787f9cd736b40dd56bdbf68
jcasoya <jcasoya@gmail.com>
2023-05-16 16:39:00 +0200

    draw circle on click event

--- 01 - Crash Course/script.js
+++ 01 - Crash Course/script.js
@@ -1,17 +1,27 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 })
 
-ctx.fillStyle = 'red';
-ctx.strokeStyle = 'red';
-ctx.lineWidth = 45;
-ctx.beginPath();
-ctx.arc(100, 100, 50, 0, Math.PI * 1.4);
-ctx.stroke();
-console.log(ctx);
- 
+const mouse = {
+    x: null,
+    y: null,
+}
+canvas.addEventListener('click', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    drawCircle();
+})
 
+function drawCircle(){
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 1.4);
+    ctx.fill();
+}
+// drawCircle();
