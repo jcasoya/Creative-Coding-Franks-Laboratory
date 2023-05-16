36abde85052504cfe7f3186cbac7da5de48a4368
jcasoya <jcasoya@gmail.com>
2023-05-16 17:13:58 +0200

    random particle animation

--- 01 - Crash Course/script.js
+++ 01 - Crash Course/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
@@ -24,8 +25,10 @@ canvas.addEventListener('mousemove', function(event){
 
 class Particle {
     constructor(){
-        this.x = mouse.x;
-        this.y = mouse.y;
+        // this.x = mouse.x;
+        // this.y = mouse.y;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.size = Math.random() * 5 + 1;
         this.speedX = Math.random() * 3 - 1.5;
         this.speedY = Math.random() * 3 - 1.5;
@@ -42,8 +45,23 @@ class Particle {
     }
 }
 
+function init(){
+    for (let i = 0; i < 100; i++){
+        particlesArray.push(new Particle());
+    }
+}
+init();
+
+function handleParticles(){
+    for (let i = 0; i < particlesArray.length; i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+}
+
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleParticles();
     requestAnimationFrame(animate);
 }
 animate();
