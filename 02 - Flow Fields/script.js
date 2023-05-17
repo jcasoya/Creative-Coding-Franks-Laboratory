32d8b6007e0189232fd66b451d3ef5687ac8ba23
jcasoya <jcasoya@gmail.com>
2023-05-17 09:40:33 +0200

    particles animated

--- 02 - Flow Fields/script.js
+++ 02 - Flow Fields/script.js
@@ -13,9 +13,15 @@ class Particle {
         this.effect = effect;
         this.x = Math.floor(Math.random() * this.effect.width);
         this.y = Math.floor(Math.random() * this.effect.height);
+        this.speedX = Math.random() * 5 -2.5;
+        this.speedY = Math.random() * 5 -2.5;
     }
     draw(context){
         context.fillRect(this.x, this.y, 10, 10);
+    }
+    update(){
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
 }
 
@@ -25,6 +31,7 @@ class Effect {
         this.height = height;
         this.particles = [];
         this.numberOfParticles = 50;
+        this.init();
     }
     init(){
         // create particles
@@ -33,13 +40,19 @@ class Effect {
         }
     }
     render(context){
-        this.particles.forEach(particle => {
+        this.particles.forEach(particle => { 
             particle.draw(context);
+            particle.update();
         })
     }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
-effect.init();
-effect.render(ctx);
 console.log(effect);
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.render(ctx);
+    requestAnimationFrame(animate);
+}
+animate();
