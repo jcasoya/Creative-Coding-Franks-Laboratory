e073952d8d40b9e4c7f38e57ff7e7f809da3cfd9
jcasoya <jcasoya@gmail.com>
2023-05-17 10:20:56 +0200

    working flow field/vector field

--- 02 - Flow Fields/script.js
+++ 02 - Flow Fields/script.js
@@ -29,9 +29,16 @@ class Particle {
         context.stroke();
     }
     update(){
-        this.angle += 0.5;
-        this.x += this.speedX + Math.sin(this.angle) * 8;
-        this.y += this.speedY + Math.cos(this.angle) * 8;
+        let x = Math.floor(this.x / this.effect.cellSize);
+        let y = Math.floor(this.y / this.effect.cellSize);
+        let index = y * this.effect.cols + x;
+        this.angle = this.effect.flowField[index];
+
+        this.speedX = Math.cos(this.angle);
+        this.speedY = Math.sin(this.angle);
+        this.x += this.speedX;
+        this.y += this.speedY;
+        
         this.history.push({x: this.x, y: this.y});
         if (this.history.length > this.maxLength){
             this.history.shift();
@@ -62,7 +69,7 @@ class Effect {
                 this.flowField.push(angle);
             }
         }
-        
+
         // create particles
         for (let i=0; i < this.numberOfParticles; i++){
             this.particles.push(new Particle(this));
