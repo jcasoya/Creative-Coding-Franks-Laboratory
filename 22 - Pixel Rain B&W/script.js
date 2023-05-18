6ea207044e550cb110c709fc0315145e639a3e37
jcasoya <jcasoya@gmail.com>
2023-05-18 23:22:10 +0200

    update method created

--- 22 - Pixel Rain B&W/script.js
+++ 22 - Pixel Rain B&W/script.js
@@ -8,4 +8,24 @@ myImage.addEventListener('load', function(){
     canvas.height = 625;
 
     ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+
+    let = particlesArray = [];
+    const numberOfParticles = 5000;
+
+    class Particle {
+        constructor(){
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+            this.speed = 0;
+            this.velocity = Math.random() * 3.5;
+            this.size = Math.random() * 1.5 + 1;
+        }
+        update() {
+            this.y += this.velocity;
+            if (this.y >= canvas.height){
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+        }
+    }
 });
